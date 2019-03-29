2105158da9417436d1955357f60124b5563c57fa
khoiruroh <alnida18@gmail.com>
2019-03-29 14:40:53 +0700

    tambah web baru

---
+++ contact.php
@@ -0,0 +1,188 @@
+<?php include('header.php');?>
+
+<!-- contact -->
+<div class="contact">
+	<div class="container">
+		<div class="spec ">
+			<h3>Contact</h3>
+				<div class="ser-t">
+					<b></b>
+					<span><i></i></span>
+					<b class="line"></b>
+				</div>
+		</div>
+		<div class=" contact-w3">	
+			<div class="col-md-5 contact-right">	
+				<img src="images/cac.jpg" class="img-responsive" alt="">
+			</div>
+			<div class="col-md-7 contact-left">
+				<h4>Contact Information</h4>
+				<p> Khoiruroh NASA</p>
+				<ul class="contact-list">
+					<li> <i class="fa fa-map-marker" aria-hidden="true"></i>jl KH Kholil no 43</li>
+					<li><i class="fa fa-envelope" aria-hidden="true"></i><a href="khoiruroh18@gmail.com">khoiruroh18@gmail.com</a></li>
+					<li> <i class="fa fa-phone" aria-hidden="true"></i>085706431885</li>
+				</ul>
+				<div id="container">
+					<!--Horizontal Tab-->
+					<div id="parentHorizontalTab">
+						<ul class="resp-tabs-list hor_1">
+							<li><i class="fa fa-envelope" aria-hidden="true"></i></li>
+							<li> <i class="fa fa-map-marker" aria-hidden="true"></i> </span></li>
+							<li> <i class="fa fa-phone" aria-hidden="true"></i></li>
+						</ul>
+						<div class="resp-tabs-container hor_1">
+							<div>
+								<form action="#" method="post">
+									<input type="text" value="Name" name="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}" required="">
+									<textarea name="Message..." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Message...';}" required="">Message...</textarea>
+									<input type="submit" value="Submit" >
+								</form>
+							</div>
+							<div>
+							</div>
+							<div>
+								<div class="map-grid">
+									<h5>Kontak Kami</h5>
+									<ul>
+										<li>Mobile No : 085706431885</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<!--Plug-in Initialisation-->
+				<script type="text/javascript">
+				$(document).ready(function() {
+					//Horizontal Tab
+					$('#parentHorizontalTab').easyResponsiveTabs({
+						type: 'default', //Types: default, vertical, accordion
+						width: 'auto', //auto or any width like 600px
+						fit: true, // 100% fit in a container
+						tabidentify: 'hor_1', // The tab groups identifier
+						activate: function(event) { // Callback function if tab is switched
+							var $tab = $(this);
+							var $info = $('#nested-tabInfo');
+							var $name = $('span', $info);
+							$name.text($tab.text());
+							$info.show();
+						}
+					});
+
+					// Child Tab
+					$('#ChildVerticalTab_1').easyResponsiveTabs({
+						type: 'vertical',
+						width: 'auto',
+						fit: true,
+						tabidentify: 'ver_1', // The tab groups identifier
+						activetab_bg: '#fff', // background color for active tabs in this group
+						inactive_bg: '#F5F5F5', // background color for inactive tabs in this group
+						active_border_color: '#c1c1c1', // border color for active tabs heads in this group
+						active_content_border_color: '#5AB1D0' // border color for active tabs contect in this group so that it matches the tab head border
+					});
+
+					//Vertical Tab
+					$('#parentVerticalTab').easyResponsiveTabs({
+						type: 'vertical', //Types: default, vertical, accordion
+						width: 'auto', //auto or any width like 600px
+						fit: true, // 100% fit in a container
+						closed: 'accordion', // Start closed if in accordion view
+						tabidentify: 'hor_1', // The tab groups identifier
+						activate: function(event) { // Callback function if tab is switched
+							var $tab = $(this);
+							var $info = $('#nested-tabInfo2');
+							var $name = $('span', $info);
+							$name.text($tab.text());
+							$info.show();
+						}
+					});
+				});
+			</script>
+				
+			</div>
+			
+		<div class="clearfix"></div>
+	</div>
+	</div>
+</div>
+<!-- //contact -->
+
+<!-- //footer-->
+<!-- tabs -->
+<script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		$(document).ready(function () {
+			$('#horizontalTab').easyResponsiveTabs({
+			type: 'default', //Types: default, vertical, accordion           
+			width: 'auto', //auto or any width like 600px
+			fit: true   // 100% fit in a container
+			});
+		});				
+	</script>
+<!-- //tabs -->
+<!-- smooth scrolling -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!-- //smooth scrolling -->
+<!-- for bootstrap working -->
+		<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+<script type='text/javascript' src="js/jquery.mycart.js"></script>
+  <script type="text/javascript">
+  $(function () {
+
+    var goToCartIcon = function($addTocartBtn){
+      var $cartIcon = $(".my-cart-icon");
+      var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
+      $addTocartBtn.prepend($image);
+      var position = $cartIcon.position();
+      $image.animate({
+        top: position.top,
+        left: position.left
+      }, 500 , "linear", function() {
+        $image.remove();
+      });
+    }
+
+    $('.my-cart-btn').myCart({
+      classCartIcon: 'my-cart-icon',
+      classCartBadge: 'my-cart-badge',
+      affixCartIcon: true,
+      checkoutCart: function(products) {
+        $.each(products, function(){
+          console.log(this);
+        });
+      },
+      clickOnAddToCart: function($addTocart){
+        goToCartIcon($addTocart);
+      },
+      getDiscountPrice: function(products) {
+        var total = 0;
+        $.each(products, function(){
+          total += this.quantity * this.price;
+        });
+        return total * 1;
+      }
+    });
+
+  });
+  </script>
+
+  
+</body>
+<?php include('footer.php');?>
+</html>
